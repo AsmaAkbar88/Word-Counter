314c4ac9ab383733e599212126662d2ddcecf926
unknown <masoomlarki297@gmail.com>
2024-06-27 20:01:35 +0500

    Counts words in input text for analysis.

---
+++ main.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node 
+// Import the "inquirer" module while is a command line interface for nodejs
+import inquirer from "inquirer";
+import chalk from "chalk";
+// Declare a constant 'answer' and assign it to the result of inquirer.prompt function
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: chalk.redBright(" \n Enter your Sentence to count the word: ")
+    }
+]);
+const words = answer.sentence.trim().split(" ");
+// Print the array of words to the console 
+console.log(words);
+// print the word  count of the sentence to the console 
+console.log(chalk.yellowBright(` \n Your Sentence Word Count is ${chalk.blueBright(words.length)}`));
